cb2b6f9e77a134245b0b0f1a26d421702f10e04f
JackBiofryd <jackbiofryd@gmail.com>
2021-04-07 21:40:55 +0200

    Fixed Length Validation

--- src/actions/todosActions.js
+++ src/actions/todosActions.js
@@ -7,7 +7,7 @@ import {
 } from '../helpers/localStorage';
 
 export const addTodo = todo => {
-	if (todo.text.length > 50) todo.text = todo.text.slice(0, 50) + '...';
+	if (todo.text.length > 80) todo.text = todo.text.slice(0, 80) + '...';
 
 	addTodoStorage(todo);
 
